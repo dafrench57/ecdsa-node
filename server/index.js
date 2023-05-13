06c0c348be7bce8e8e29767d89ae4a336ba7f3b1
dafrench57 <sinandogan57@gmail.com>
2023-05-13 23:29:33 +0200

    succesfful

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "049ef71bb1de8ac4911aba930def2d70636db50fe67f1314d6e1b729ad57e8a111f8bfcc2b2d8c17fd81d389a6b58050d5fc3222f572e23fd1ca6a4edfc78c0ac1": 100,
+  "045e9bcc42adc7bbaeb8625910ae69db0d57a91107a0bcdd0270b2d360579d539537c70fba097734b3adf5d996117260069279e05495defb587f6f5777cd278070": 50,
+  "04dabc9b3992c64c861014cb2a429f7888690254a0af3abcd7f88bbc31b16d2e0b5b674838dbb55a7e97aaee39f1601269346322c2a87fd2085981644bde658fd5": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
